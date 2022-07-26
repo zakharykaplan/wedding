5f7ffecbb5cdfa7df2732094bf26e953c80d3553
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-07-25 22:48:00 -0400

    feat(server): hello axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::response::{Html, IntoResponse};
+use axum::routing::get;
+use axum::{Router, Server};
+use log::info;
+
+#[tokio::main]
+async fn main() {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // Build our application with a route
+    let app = Router::new().route("/", get(index));
+
+    // Run it
+    let addr = "[::]:3000".parse().unwrap();
+    info!("listening on {addr}");
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> impl IntoResponse {
+    Html("<h1>Hello, world!</h1>")
 }
