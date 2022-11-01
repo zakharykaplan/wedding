bbbcfe66ece201dca980505ff2e6900db4c9d557
Zakhary Kaplan <me@zakhary.dev>
2022-11-01 15:00:00 -0400

    feat(server): get port from cli, env

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
 use std::io;
+use std::net::SocketAddr;
 
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get_service;
 use axum::{Router, Server};
+use clap::Parser;
 use log::info;
 use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
+
+/// Hannah & Zakhary's wedding server.
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// Port to listen for connections.
+    #[arg(short, long)]
+    #[arg(default_value_t = 3000)]
+    #[arg(env = "PORT")]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
     // Initialize tracing
     tracing_subscriber::fmt::init();
+    // Parse args
+    let args = Args::parse();
 
     // Build our application with a route
     let app = Router::new()
@@ -19,7 +34,7 @@ async fn main() {
         .layer(TraceLayer::new_for_http());
 
     // Run it
-    let addr = "[::]:3000".parse().unwrap();
+    let addr = SocketAddr::from(([0; 8], args.port));
     info!("listening on {addr}");
     Server::bind(&addr)
         .serve(app.into_make_service())
